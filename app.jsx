fa42ee70958aefa3c4eb39fe1d09009568755e29
Gavlooth <gavlooth@gmail.com>
2016-09-28 12:38:48 +0300

    dungeon generating algorithm  update

--- app.jsx
+++ app.jsx
@@ -1,20 +1,82 @@
 import Path from './node_modules/paths-js/path.js';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as lodash from 'lodash';
+import _ from 'lodash';
 const width = 1000,
     height = 500;
 
+function dist(x, y) {
+    return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+}
+function moveAway(beta, alpha) {
+    const dify = beta.y - alpha.y - alpha.height;
+    const difx = beta.x - alpha.x - alpha.width;
+    let newBeta = beta;
+    if ((dify <= 0) && (difx <= 0))
+        if (difx > dify)
+            newBeta.x = beta.x - difx +10;
+        else
+            newBeta.y = beta.y - dify +10 ;
 
 
-function GenerateDungeon(){
-const Rnum = 15;
-const minDim = {x:width/(15*3),y:height/(15*3)}
-const maxDim = {x:width/10,y:height/10}
 
-let dungeon = (_.times(arrayLength, _.constant(null)))
+    return newBeta;
+
+}
+
+function GenerateDungeon(size, width, height) {
+
+    const minDim = {
+        x: width / (15 * 3),
+        y: height / (15 * 3)
+    };
+    const maxDim = {
+        x: width / 10,
+        y: height / 10
+    };
+    let IsDisjoint = false;
+
+    let dungeon = (_.times(size, (a) => {
+        return {
+            x: (_.random(width) - width / 2),
+            y: (_.random(height) - height / 2),
+            width: Math.floor(_.random(minDim.x, maxDim.x, true)),
+            height: Math.floor(_.random(minDim.y, maxDim.y, true))
+        }
+    })).sort((alpha, beta) => {
+        return dist(alpha.x, alpha.y) - dist(beta.x, beta.y)
+    })
+     for (let j = 1; j < dungeon.length; j++)
+        for (let i = 0; i < j; i++)
+            dungeon[j] = (moveAway(dungeon[j], dungeon[i]));
 
 
+
+    return dungeon;
+
+};
+
+function DrawRoom(room) {
+    const {x, y, width, height} = room;
+    return Path().moveto(x, y).hlineto(x + width).vlineto(y + height).hlineto(x).closepath();
+
+}
+
+class Hello extends React.Component {
+    render() {
+        let dungeon = GenerateDungeon(30, 2000, 1000);
+        let rooms = dungeon.map(x => DrawRoom(x));
+
+        return (
+            <svg width={width} height={height} viewBox={"-1000 -500 2000 1000"} preserveAspectRatio={"none"} style={{
+                background: "white"
+            }}>
+                {rooms.map(x => <g>
+                    <path d={x.print()} fill="none" stroke="blue"/>
+                </g>)}
+            </svg>
+        )
+    }
 };
 
 /*
@@ -50,19 +112,6 @@ function GenerateRoom(center, dim, exits) {
 
 }
 */
-let path = Path().moveto(10, 20).lineto(30, 50).lineto(25, 28).qcurveto(27, 30, 32, 27).closepath();
-
-class Hello extends React.Component {
-    render() {
-        return (
-            <svg width={width} height={height} viewBox={"-1000 -500 2000 1000"} preserveAspectRatio={"none"} style={{
-                background: "white"
-            }}>
-                <path d={path.print()} fill="blue"/>
-            </svg>
-        )
-    }
-};
 
 ReactDOM.render(
     <Hello/>, document.getElementById('App'));
