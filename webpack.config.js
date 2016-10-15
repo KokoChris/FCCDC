8ad3fe7cadd64dd748bef2aed1a331da268e4d2e
Chris Kokolios <kokolios1986@yahoo.gr>
2016-10-16 00:03:46 +0300

    tests

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports =  {
   entry: [
     // 'eventsource-polyfill', // necessary for hot reloading with IE
     // 'webpack-hot-middleware/client?reload=true', //note that it reloads the page if hot module reloading fails.
-    path.resolve(__dirname, 'src/index.js')
+    './src/index.js'
   ],
   target: 'web',
   output: {
