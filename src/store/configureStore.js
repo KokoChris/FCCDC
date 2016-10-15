85cc66866363fd91d4de19a038972ff662874a65
Chris Kokolios <kokolios1986@yahoo.gr>
2016-10-15 23:17:46 +0300

    minor

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,4 +1,4 @@
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware,compose} from 'redux';
 import rootReducer from '../reducers';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 import thunk from 'redux-thunk';
@@ -10,6 +10,6 @@ export default function configureStore(initialState) {
     return createStore(
         rootReducer,
         initialState,
-        composeEnhancers(applyMiddleware(thunk,reduxImmutableStateInvariant()))
+        composeEnhancers(applyMiddleware(thunk,reduxImmutableStateInvariant(),compose))
     );
 }
